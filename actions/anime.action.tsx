411b37d3fd0635a3e1cc5d5153a996da9ec37bf6
masumbillah360 <masumbillah6584@gmail.com>
2024-05-03 10:40:17 +0600

    updated limit size to get anime

--- actions/anime.action.tsx
+++ actions/anime.action.tsx
@@ -3,7 +3,7 @@ import AnimeCard, { AnimeProp } from '@/components/AnimeCard';
 
 export const fetchAnime = async (page: number) => {
     const response = await fetch(
-        `https://shikimori.one/api/animes?page=${page}&limit=50`
+        `https://shikimori.one/api/animes?page=${page}&limit=8`
     );
     const data = await response.json();
     if (data.length == 1) {
